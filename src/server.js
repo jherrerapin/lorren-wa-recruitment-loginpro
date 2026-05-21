b4766e13938fea80a8b3999dad9daca5ee9bf74b
jherrerapin <37624185+jherrerapin@users.noreply.github.com>
2026-05-20 22:26:42 -0500

    feat: registrar rutas de estadisticas por cliente

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import { adminRouter } from './routes/admin.js';
 import { botKnowledgeCrudRouter } from './routes/botKnowledgeCrud.js';
 import { locationsRouter } from './routes/locations.js';
 import { dispatchDashboardMetricsRouter } from './routes/dispatchDashboardMetrics.js';
+import { dispatchClientStatsRouter } from './routes/dispatchClientStats.js';
 import { dispatchOpsExtrasRouter } from './routes/dispatchOpsExtras.js';
 import { dispatchBridgeRouter } from './routes/dispatchBridge.js';
 import { publicDispatchClientRouter } from './routes/publicDispatchClient.js';
@@ -291,6 +292,7 @@ app.use('/webhook', webhookRouter(prisma));
 app.use('/admin/bot-knowledge', botKnowledgeCrudRouter(prisma));
 app.use('/operaciones', publicDispatchClientRouter());
 app.use('/admin/operaciones', dispatchDashboardMetricsRouter(prisma));
+app.use('/admin/operaciones', dispatchClientStatsRouter(prisma));
 app.use('/admin/operaciones', dispatchOpsExtrasRouter(prisma));
 app.use('/admin/operaciones', dispatchBridgeRouter());
 app.use('/admin', (req, res, next) => {
