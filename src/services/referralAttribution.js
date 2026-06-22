55f10129ec68fd3a825c3168a77c94c702576376
jherrerapin <37624185+jherrerapin@users.noreply.github.com>
2026-06-22 15:37:43 -0500

    feat(loren-v2): connect origin runtime

--- src/services/referralAttribution.js
+++ src/services/referralAttribution.js
@@ -1,6 +1,6 @@
-const noop = () => (_req, _res, next) => next();
+import { runtime } from './originRuntime.js';
 
-export { noop as \u{72}\u{65}\u{66}\u{65}\u{72}\u{72}\u{61}\u{6c}\u{41}\u{74}\u{74}\u{72}\u{69}\u{62}\u{75}\u{74}\u{69}\u{6f}\u{6e}\u{4d}\u{69}\u{64}\u{64}\u{6c}\u{65}\u{77}\u{61}\u{72}\u{65} };
+export const referralAttributionMiddleware = runtime;
 
 export function disabledLorenV2AttributionService() {
   return { enabled: false };
