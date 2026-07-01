d4f686952e6b29d04a4b48139bbf5ca87de8d96f
jherrerapin <37624185+jherrerapin@users.noreply.github.com>
2026-07-01 18:42:50 -0500

    Add recruitment statistics route entry point

---
+++ src/routes/recruitmentStats.js
@@ -0,0 +1,4 @@
+// Temporary business-named entry point for recruitment statistics.
+// The implementation will be moved here progressively from lorenV2.js.
+
+export { default } from './lorenV2.js';
