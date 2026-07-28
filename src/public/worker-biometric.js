8fcc6482d9d2691611ad1f60d445e8a01f3668a2
jherrerapin <37624185+jherrerapin@users.noreply.github.com>
2026-07-28 13:08:46 -0500

    fix(asistencia): cargar estabilizador biométrico móvil

--- src/public/worker-biometric.js
+++ src/public/worker-biometric.js
@@ -1,4 +1,5 @@
 'use strict';
 
 document.write('<script src="/public/worker-biometric-core.js"><\/script>');
+document.write('<script src="/public/worker-biometric-mobile.js"><\/script>');
 document.write('<script src="/public/worker-portal-hardening.js"><\/script>');
