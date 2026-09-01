3170e46f0a24d08f3cabb4a1da4aa75fdd9c57d9
jherrerapin <37624185+jherrerapin@users.noreply.github.com>
2026-09-01 13:12:29 -0500

    test(ci): aislar regresion de turnos diurnos

--- test/adminSession.test.js
+++ test/adminSession.test.js
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
 import fs from 'node:fs';
-import './payrollRemuneratedCounters.test.js';
+import './payrollDayShiftCountFocal.test.js';
 
 import {
   ADMIN_SESSION_DEFAULTS,
